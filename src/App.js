95e26b586dbbc3b5a62034fd4f5cf2881f3815bf
Максим Стрельников <maksim_stv@mail.ru>
2023-04-11 17:01:24 +0300

    init-2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import { useEffect } from "react";
 
-function getJSON(){
-  fetch('https://testspringboot-production.up.railway.app/json')
-  .then(response => response.json())
-  .catch(e => console.log(e))
+async function getJSON(){
+
+  try{
+    const response = await fetch('https://testspringboot-production.up.railway.app/json')
+    console.log(response)
+    const json = await response.json()
+    console.log(json)
+  }
+  catch(e){
+    console.log(e)
+  }
 }
 
 function App() {
