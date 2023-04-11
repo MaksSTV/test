8b291931be0183732d35845c2386e84ad340cd12
Максим Стрельников <maksim_stv@mail.ru>
2023-04-11 16:37:07 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+
+function getJSON(){
+  fetch('https://testspringboot-production.up.railway.app/json')
+  .then(response => response.json())
+  .then(json => console.log(json))
+  .catch(e => console.log(e))
+}
 
 function App() {
+
+  useEffect(()=>{
+    getJSON()
+  }, [])
+
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello!
     </div>
   );
 }
